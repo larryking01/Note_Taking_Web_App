af15fa5d6f13f80f8ff965f5848f7221fa65c4f6
larry <larryking8118@gmail.com>
2025-06-25 10:54:09 +0000

    feature: implemented user specific notes view

--- src/app/components/display-notes/display-notes.ts
+++ src/app/components/display-notes/display-notes.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Navbar } from '../navbar/navbar';
+import { NoteCrudService } from '../../services/notesCRUD/note-crud-service';
+import { Observable } from 'rxjs';
+import { NoteInterface } from '../../models/noteInterface';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-display-notes',
-  imports: [Navbar],
+  imports: [Navbar, CommonModule],
   templateUrl: './display-notes.html',
   styleUrl: './display-notes.scss'
 })
-export class DisplayNotes {
+export class DisplayNotes implements OnInit {
+
+  notesService = inject( NoteCrudService )
+
+  allNotes$: Observable<NoteInterface[]> = this.notesService.getUserNotesRealTime()
+
+  AllNotesArray: NoteInterface[] = []
+
+
+  ngOnInit(): void {
+    this.notesService.getUserNotesRealTime().subscribe({
+      next: ( data => { 
+        this.AllNotesArray = data
+        console.log("all notes fetched = ", this.AllNotesArray)
+       })
+    })
+  }
+
+
 
 }
--- src/app/services/notesCRUD/note-crud-service.ts
+++ src/app/services/notesCRUD/note-crud-service.ts
@@ -1,9 +1,11 @@
 import { Injectable, inject } from '@angular/core';
 import { NoteInterface } from '../../models/noteInterface';
 import { BehaviorSubject } from 'rxjs'
-import { Firestore, collection, addDoc, CollectionReference } from '@angular/fire/firestore';
-import { Auth } from '@angular/fire/auth';
+import { Firestore, collection, addDoc, CollectionReference, getDocs, collectionData } from '@angular/fire/firestore';
+import { Auth, authState } from '@angular/fire/auth';
+import { Observable, switchMap, of } from 'rxjs';
 import { AuthService } from '../authentication/auth-service';
+import { query } from 'firebase/firestore';
 
 
 
@@ -20,7 +22,6 @@ export class NoteCrudService {
   // currentUserEmail: string | null | undefined = null;
 
   
-  private notesReference: CollectionReference = collection( this.firestore, 'Notes') as CollectionReference;
 
 
 
@@ -44,20 +45,56 @@ export class NoteCrudService {
       throw new Error('User not authenticated')
     }
     else {
+      const notesReference: CollectionReference = collection( this.firestore, `Users/${ currentUser.email }/notes`) as CollectionReference;
       const newNote: NoteInterface = {
         ...note
       }
 
-      await addDoc( this.notesReference, newNote )
+      await addDoc( notesReference, newNote )
       console.log("note created = ", newNote)
     }
     
   }
 
 
-  fetchAllNotes() {
+  async getUserNotesOnce(): Promise<NoteInterface[]> {
+    const user = this.auth.currentUser
+    if(!user?.email) {
+      return []
+    }
+    else {
+      const notesReference: CollectionReference = collection(this.firestore, `Users/${ user.email }/notes`)
+      const querySnapshot = await getDocs( notesReference )
+
+      return querySnapshot.docs.map( doc => ({ id: doc.id, ...doc.data() }) as NoteInterface )
+    }
 
   }
+
+
+  getUserNotesRealTime(): Observable<NoteInterface[]> {
+    // const user = this.auth.currentUser;
+    // if(!user?.email) {
+    //   return new Observable()
+    // }
+    // else {
+    //   const notesReference: CollectionReference = collection(this.firestore, `Users/${ user.email }/notes`)
+    //   return collectionData( notesReference, { idField: 'id' }) as Observable<NoteInterface[]>
+    // }
+
+    return authState( this.auth ).pipe(
+      switchMap( user => {
+        if(!user?.email) {
+          return of([])
+        }
+        else {
+          const notesReference: CollectionReference = collection(this.firestore, `Users/${ user.email }/notes`)
+          return collectionData( notesReference, { idField: 'id' }) as Observable<NoteInterface[]>
+        }
+      })
+    )
+  }
+
 
 
   fetchNote( note: NoteInterface ) {
