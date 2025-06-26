e0e68aeb2581294260de55cccbf88f4c7936d755
larry <larryking8118@gmail.com>
2025-06-26 21:38:26 +0000

    feature: implement partial theming

--- src/app/sidebar/sidebar.ts
+++ src/app/sidebar/sidebar.ts
@@ -1,20 +1,26 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
-
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [ RouterModule ],
+  imports: [ RouterModule, CommonModule ],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss'
 })
-export class Sidebar  {
+export class Sidebar implements OnInit {
 
   router = inject( Router )
 
-  fontType: string = '';
+  fontType: string = 'sans';
   
   themeType: string = '';
+
+  lightMode: boolean = true;
+
+  toggleTheme() {
+    this.lightMode = !this.lightMode
+  }
 
   navigateToCreateNote() {
     this.router.navigate(['create'])
@@ -40,30 +46,44 @@ export class Sidebar  {
     this.router.navigate([ route ])
   }
 
-
-  setFontSerif() {
-    this.fontType = 'serif'
+  setFont(font: string) {
+    this.fontType = font
     console.log('font type = ', this.fontType)
     document.body.classList.remove('serif', 'sans-serif', 'monospace')
     document.body.classList.add( this.fontType )
   }
 
-  setFontSansSerif() {
-    this.fontType = 'sans-serif'
-    console.log('font type = ', this.fontType)
-    document.body.classList.remove('serif', 'sans-serif', 'monospace')
-    document.body.classList.add( this.fontType )
+
+  ngOnInit(): void {
+    this.setFont( this.fontType )
   }
 
-  setFontMonospace() {
-    this.fontType = 'monospace'
-    console.log('font type = ', this.fontType)
-    document.body.classList.remove('serif', 'sans-serif', 'monospace')
-    document.body.classList.add( this.fontType )
-  }
+  // setFontSerif() {
+  //   this.fontType = 'serif'
+  //   console.log('font type = ', this.fontType)
+  //   document.body.classList.remove('serif', 'sans-serif', 'monospace')
+  //   document.body.classList.add( this.fontType )
+  // }
+
+
+
+  // setFontSansSerif() {
+  //   this.fontType = 'sans-serif'
+  //   console.log('font type = ', this.fontType)
+  //   document.body.classList.remove('serif', 'sans-serif', 'monospace')
+  //   document.body.classList.add( this.fontType )
+  // }
+
+  // setFontMonospace() {
+  //   this.fontType = 'monospace'
+  //   console.log('font type = ', this.fontType)
+  //   document.body.classList.remove('serif', 'sans-serif', 'monospace')
+  //   document.body.classList.add( this.fontType )
+  // }
 
 
   setThemeDark() {
+    this.toggleTheme()
     this.themeType = 'dark'
     document.body.classList.add('dark')
     console.log( document.body.classList )
@@ -72,6 +92,7 @@ export class Sidebar  {
 
 
   setThemeLight() {
+    this.toggleTheme()
     this.themeType = 'light'
     document.body.classList.remove('dark')
     // document.body.classList.add('light')
